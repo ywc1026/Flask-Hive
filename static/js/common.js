f096dd33b5b671207b48a9ae11fe2b93c548324f
Guinsoo <ywc1026@163.com>
2017-10-11 00:25:58 -0700

    change margin

--- static/js/common.js
+++ static/js/common.js
@@ -18,26 +18,91 @@ function init_chart(chartlist){
 }
 
 
+
 function flushchart(elem) {
 
-    var option = {
-        title: {
-            text: 'ECharts 入门示例'
+    option = {
+    title: {
+        text: 'The Price of Stock',
+        subtext: '0000338'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross'
+        }
+    },
+    toolbox: {
+        show: true,
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis:  {
+        type: 'category',
+        boundaryGap: false,
+        data: ['09:30', '09:40', '09:50', '10:00', '10:10', '10:20', '10:30', '10:40', '10:50', '11:00', '11:10', '11:20', '11:30', '13:00', '13:10', '13:20', '13:30', '13:40', '13:50', '14:00']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel: {
+            formatter: '{value} $'
         },
-        tooltip: {},
-        legend: {
-            data: ['销量']
-        },
-        xAxis: {
-            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [{
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
+        axisPointer: {
+            snap: true
+        }
+    },
+    visualMap: {
+        show: false,
+        dimension: 0,
+        pieces: [{
+            lte: 6,
+            color: 'red'
+        }, {
+            gt: 6,
+            lte: 8,
+            color: 'green'
+        }, {
+            gt: 8,
+            lte: 14,
+            color: 'green'
+        }, {
+            gt: 14,
+            lte: 17,
+            color: 'red'
+        }, {
+            gt: 17,
+            color: 'green'
         }]
-    };
+    },
+    series: [
+        {
+            name:'Price',
+            type:'line',
+            smooth: true,
+            data: [30, 28, 25, 26, 27, 30, 55, 50, 40, 39, 38, 39, 40, 50, 60, 75, 80, 70, 60, 40],
+            markArea: {
+                data: [ [{
+                    name: 'AskVolume',
+                    xAxis: '09:40'
+                }, {
+                    xAxis: '10:00'
+                }], [{
+                    name: 'AskVolume',
+                    xAxis: '10:50'
+                }, {
+                    xAxis: '11:30'
+                }], [{
+                    name: 'AskVolume',
+                    xAxis: '13:30'
+                }, {
+                    xAxis: '14:00'
+                }] ]
+            }
+        }
+    ]
+};
+
 
 
     // 使用刚指定的配置项和数据显示图表。
