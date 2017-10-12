04cce706f1d1e702f8d07460dd66505c1b3168f4
Guinsoo <ywc1026@163.com>
2017-10-12 05:20:57 -0700

    gitignore

--- config.py
+++ config.py
@@ -10,3 +10,18 @@ MYSQL_DATABASE = 'system'
 
 WEB_HOST = '127.0.0.1'
 WEB_PORT = 8000
+
+
+'logger config'
+MAIL_HOST = 'smtp.qq.com'
+MAIL_POST = '587'
+MAIL_PASSWORD = ''
+MAIL_USERNAME = '1136937352@qq.com'
+MAIL_FROM = '1136937352@qq.com'
+MAIL_TOLIST = ['ywc1026@163.com']
+
+LOGGER_PATH = "/home/ywc/PycharmProjects/system/my.log"
+
+
+
+
--- logger.py
+++ logger.py
@@ -1,4 +1,55 @@
+import logging
+from logging import StreamHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
+import config
+
+
 class LoggerConfig(object):
 
     def __init__(self):
         self.logger_name = 'root'
+
+        logging.basicConfig(filename=config.LOGGER_PATH, level=logging.DEBUG)
+
+        self.add_stream_handler()
+        self.add_file_handler()
+        self.add_email_handler()
+
+    def into(self, msg):
+
+        logging.getLogger(self.logger_name).info(msg)
+
+    def add_email_handler(self):
+
+        handler = SMTPHandler(mailhost=config.MAIL_HOST, fromaddr=config.MAIL_FROM, toaddrs=config.MAIL_TOLIST, subject='server error', credentials=(config.MAIL_USERNAME, config.MAIL_PASSWORD), secure=())
+        handler.setLevel(logging.ERROR)
+        handler.setFormatter(logging.Formatter('''
+                Message type: %(levelname)s
+                Location: %(pathname)s: %(lineno)d
+                Module: %(module)s
+                Function: %(funcName)s
+                Time: '%(asctime)s
+                Message:
+                    $(message)s'''))
+        logging.getLogger(self.logger_name).addHandler(handler)
+
+    def add_stream_handler(self):
+
+        handler = StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s [%(pathname)s:%(lineno)d]'))
+        logging.getLogger(self.logger_name).addHandler(handler)
+
+    def add_file_handler(self):
+
+        file_handler = RotatingFileHandler(config.LOGGER_PATH, maxBytes=1024*1024*500, delay=False, backupCount=20)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s [%(pathname)s:%(lineno)d]'))
+        logging.getLogger(self.logger_name).addHandler(file_handler)
+
+
+if __name__ == "__main__":
+
+    logger = LoggerConfig()
+
+    logger.into("haha")
